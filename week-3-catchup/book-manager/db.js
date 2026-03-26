093454f9ee088c5ae1b32399b8301622f9964565
abascharles <abascharlesbenard@gmail.com>
2026-03-26 10:35:36 +0300

    Create a new mongo db connection

--- week-3-catchup/book-manager/db.js
+++ week-3-catchup/book-manager/db.js
@@ -2,3 +2,8 @@ const { MongoClient } = require('mongodb');
 
 const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
+
+async function connectDB() {
+  await client.connect();
+  return client.db('bookstore').collection('books');
+}
